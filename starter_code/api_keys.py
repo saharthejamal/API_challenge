81eece3866cc6eb9b4eb26015190e1e8ac70ab51
saharthejamal <87100578+saharthejamal@users.noreply.github.com>
2021-09-15 08:13:03 -0400

    Delete api_keys.py

--- starter_code/api_keys.py
+++
@@ -1,6 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "87a515ce530bf195880e8fe98a857295"
-
-# Google API Key
-g_key = "AIzaSyCxMLwfUOQFv5_jP4cXf4yateGMxORq_kE"
-#   <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script>
