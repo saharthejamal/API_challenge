d3ba549e15636e3f2f236b07ac7c6d6a28ebd955
saharthejamal <jamalsahar@yahoo.com>
2021-09-15 08:07:37 -0400

    commited

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "87a515ce530bf195880e8fe98a857295"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCxMLwfUOQFv5_jP4cXf4yateGMxORq_kE"
+#   <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script>
